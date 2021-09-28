9eb6c9b363271c2e1618d7ec68a5a0eb8015ab80
Gustavo Silva <gustagui.dev@gmail.com>
2021-09-27 21:10:07 -0300

    first commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,8 @@
+function sorteio(){
+    const min = document.getElementById('minimo').value;
+    const max = document.getElementById('maximo').value;
+
+    const sort = Math.floor((Math.random() * max) +min);
+
+    document.getElementById('resultado').innerHTML=sort;;
+}
